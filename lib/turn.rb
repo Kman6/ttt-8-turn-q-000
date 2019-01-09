7b8e22e4a812ac45235b60914cb94de6996782d4
Kalil Salkey <ksalkey@gmail.com>
2019-01-09 20:11:54 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -33,4 +33,6 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     puts "Please enter 1-9:"
     user_input = gets.strip
     input_to_index(user_input)
+    
+     display_board(board)
     end 
