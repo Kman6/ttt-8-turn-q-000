087c411f089e57df9bd5ea90a2f4a4da5636ac49
Kalil Salkey <ksalkey@gmail.com>
2019-01-09 20:13:45 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -34,6 +34,7 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
     user_input = gets.strip
     input_to_index(user_input)
     
-     move(board, z, value = "X") 
+    
+    
      display_board(board)
-    end 
+  end 
