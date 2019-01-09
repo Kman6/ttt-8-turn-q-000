2a4427afc033c570ea4b1bb57073be7950650010
Kalil Salkey <ksalkey@gmail.com>
2019-01-09 20:05:30 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,39 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+  def display_board(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+  
+  def valid_move?(board, index)
+  
+    if index.between?(0,8) && !position_taken?(board, index)
+      return true
+    else 
+      return false
+    end
+  end
+  
+  def position_taken?(board, index)
+    board[index] != " "
+  end
+  
+  def input_to_index(user_input)
+    index= user_input.to_i - 1
+  end
+  
+  def move(board, z, value = "X")
+  board[z] = value
+  end
+  
+  def turn(board)
+    puts "Please enter 1-9:"
+    user_input = gets.strip
+    input_to_index(user_input)
+    index. between (0, 8) && !position_taken?(board, index)
+    
+  end
+end 
